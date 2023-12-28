66f8920ae0bd16202a0cb5a97627dc83f298bb77
NgocHieu010503 <154217329+NgocHieu010503@users.noreply.github.com>
2023-12-28 15:30:40 +0700

    Fixture.java

---
+++ Fixture.java
@@ -0,0 +1,46 @@
+package com.yourcompany.bundesliga.model;
+
+public class Fixture {
+    private String home;
+    private String away;
+    private String date;
+    private String result;
+
+    public Fixture(String home, String away, String date) {
+        this.home = home;
+        this.away = away;
+        this.date = date;
+    }
+
+    public String getHome() {
+        return home;
+    }
+
+    public void setHome(String home) {
+        this.home = home;
+    }
+
+    public String getAway() {
+        return away;
+    }
+
+    public void setAway(String away) {
+        this.away = away;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+}
